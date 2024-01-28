fd85d32db809c0a6ce0dc9b6a5d1ba83bac24aa7
AlexDyakonov <shram.monolit@mail.ru>
2024-01-28 21:02:46 +0300

    Added new models. YOU NEED CLEAR ALL BD DATA TO USE THIS COMMIT

--- backend/apps/api/admin.py
+++ backend/apps/api/admin.py
@@ -1,22 +1,54 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
 
-from .models import Coordinate, PlayerLobby, QuestPoint
+from .models import (
+    Coordinate,
+    LobbyPhoto,
+    PlayerLobby,
+    PlayerLobbyMembership,
+    QuestCompleted,
+    QuestPoint,
+    QuestTask,
+)
 
 
-# Register your models here.
+# Inline класс для Membership модели
+class PlayerLobbyMembershipInline(admin.TabularInline):
+    model = PlayerLobbyMembership
+    extra = 1
+
+
+# Inline класс для фотографий лобби
+class LobbyPhotoInline(admin.TabularInline):
+    model = LobbyPhoto
+    extra = 1
+
+
+# Inline класс для завершенных квестов
+class QuestCompletedInline(admin.TabularInline):
+    model = QuestCompleted
+    extra = 1
+
+
+# Админ класс для координат
 @admin.register(Coordinate)
 class CoordinateAdmin(admin.ModelAdmin):
     list_display = ("latitude", "longitude")
     search_fields = ("latitude", "longitude")
 
 
+# Админ класс для задач квеста
+@admin.register(QuestTask)
+class QuestTaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "description"]
+
+
+# Админ класс для точек квеста
 @admin.register(QuestPoint)
 class QuestPointAdmin(admin.ModelAdmin):
     list_display = ("title", "description_short", "location", "product_image")
     search_fields = ("title", "description")
     list_filter = ("location",)
-    raw_id_fields = ("location",)
 
     def description_short(self, obj):
         return (
@@ -31,8 +63,20 @@ class QuestPointAdmin(admin.ModelAdmin):
         return "No Image"
 
 
+# Админ класс для лобби игрока
 @admin.register(PlayerLobby)
 class PlayerLobbyAdmin(admin.ModelAdmin):
-    list_display = ["id", "host", "created_at"]
-    list_filter = ["host"]
-    search_fields = ["host__username"]
+    list_display = ["id", "host", "created_at", "started_at", "duration"]
+    inlines = [PlayerLobbyMembershipInline, LobbyPhotoInline, QuestCompletedInline]
+
+
+# Админ класс для фотографий лобби
+@admin.register(LobbyPhoto)
+class LobbyPhotoAdmin(admin.ModelAdmin):
+    list_display = ["id", "lobby", "image", "upload_time"]
+
+
+# Админ класс для завершенных квестов
+@admin.register(QuestCompleted)
+class QuestCompletedAdmin(admin.ModelAdmin):
+    list_display = ["id", "lobby", "user", "task"]
