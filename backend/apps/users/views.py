75fcd98f9bb0eb3a6e54a66ac67e0e19bd7bbdca
AlexDyakonov <shram.monolit@mail.ru>
2024-02-02 01:00:21 +0300

    Added upload image function using content

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import CustomUser
@@ -16,7 +16,25 @@ class CustomUserDetailView(RetrieveUpdateAPIView):
     serializer_class = CustomUserSerializer
     lookup_field = "id"
 
+    def patch(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CustomUserListView(ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
