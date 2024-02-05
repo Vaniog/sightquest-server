78be093e4a4c9feb5229dac6ba338e49bf309840
Vaniog <forvantar@gmail.com>
2024-02-05 22:09:49 +0300

    Telegram quick fix

--- backend/apps/mailer/models.py
+++ backend/apps/mailer/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Subscriber(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(max_length=255)
-    telegram = models.CharField(max_length=255)
+    telegram = models.CharField(max_length=255, null=True)
     approved = models.BooleanField(default=False)
