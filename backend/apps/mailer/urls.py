f7c5b74dc581f41f88799f10117746512438e570
Vaniog <forvantar@gmail.com>
2024-02-21 00:36:01 +0300

    Rename send-mailing to mailings/send

--- backend/apps/mailer/urls.py
+++ backend/apps/mailer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MailingCreateView, SubscriberListCreateView, MailingListView, MailingRetrieveView
 
 urlpatterns = [
-    path('send-mailing/', MailingCreateView.as_view(), name='send-mailing'),
+    path('mailings/send/', MailingCreateView.as_view(), name='send-mailing'),
     path('mailings/', MailingListView.as_view(), name='get-mailings'),
     path('mailings/<int:pk>/', MailingRetrieveView.as_view(), name='get-mailing-by-id'),
     path('subscribers/', SubscriberListCreateView.as_view(), name='subscriber-list-create'),
