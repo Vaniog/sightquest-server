5812a763bf6807a6e263218f10d3c1d41a14a3dd
AlexDyakonov <shram.monolit@mail.ru>
2024-02-24 23:13:23 +0300

    Removed signed url for images. Now public access

--- backend/backend/yandex_s3_storage.py
+++ backend/backend/yandex_s3_storage.py
@@ -6,3 +6,8 @@ from backend.settings import YANDEX_BUCKET_NAME
 class ClientDocsStorage(S3Boto3Storage):
     bucket_name = YANDEX_BUCKET_NAME
     file_overwrite = False
+
+    def url(self, name, parameters=None, expire=None):
+        url = super().url(name, parameters, expire)
+        pure_url = url.split("?")[0]
+        return pure_url
