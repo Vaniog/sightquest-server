e355709e2cf6b7d82a7536564ae24e3ddfe18ee3
AlexDyakonov <shram.monolit@mail.ru>
2024-03-04 10:54:31 +0300

    Fixed. Now Everything saves

--- backend/apps/mailer/admin.py
+++ backend/apps/mailer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Subscriber
+from .models import Mail, Mailing, Subscriber
 
 
 @admin.register(Subscriber)
@@ -9,3 +9,17 @@ class SubscriberAdmin(admin.ModelAdmin):
     list_filter = ("approved",)
     search_fields = ("email", "telegram")
     list_editable = ("approved",)
+
+
+@admin.register(Mail)
+class MailAdmin(admin.ModelAdmin):
+    list_display = ("subject", "message", "html_message", "text")
+    search_fields = ["subject", "message"]
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("from_email", "created_at", "status")
+    list_filter = ("status",)
+    search_fields = ["from_email"]
+    date_hierarchy = "created_at"
--- backend/apps/mailer/urls.py
+++ backend/apps/mailer/urls.py
@@ -8,7 +8,6 @@ from .views import (
 )
 
 urlpatterns = [
-    path("old/mailings/send/", MailingCreateView.as_view(), name="send-mail"),
     path("mailings/send/", MailingCreateView.as_view(), name="send-mailing"),
     path("mailings/", MailingListView.as_view(), name="get-mailings"),
     path("mailings/<int:pk>/", MailingRetrieveView.as_view(), name="get-mailing-by-id"),
